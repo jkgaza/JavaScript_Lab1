4b6be57aaf180fcf4e89186bcdb9ad18a2b96253
Jordan Gaza <Gaza>
2019-11-05 12:07:44 -0500

    Trying to figure out if else statement

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const myName = "Jordan Gaza";
-const age = 26;
-const birthday = "December 2nd";
-console.log(
-  "Hello, my name is " + myName + ". I am currently " + age + " years old and my birthday is on " + birthday
-);
+const name = "Jordan Gaza";
+const city = "Grand Rapids, Michigan";
+function greeting(a) {
+  if (city = Detroit, Michigan) {
+    return "Hello, my name is " + name + "and I am a student at Grand Circus in Grand Rapids, Michigan";
+  } else {
+    return "Hello, my name is " + name + "and I am a student at Grand Circus in Detroit, Michigan";
+  }
+}
+console.log(greeting());;
+
+// const myName = "Jordan Gaza";
+// const age = 26;
+// const birthday = "December 2nd";
+// console.log(
+//   "Hello, my name is " + myName + ". I am currently " + age + " years old and my birthday is on " + birthday
+// );
 
 const interestingFacts1 = "I was born in Big Rapids, Michigan. ";
 const interestingFacst2 = "I graduated from Kendall College of Art and Design. ";
