a6b1c75128cd6c191187826651eb75c8b13f60d6
Jordan Gaza <Gaza>
2019-11-05 11:23:05 -0500

    Got strings down, figuring out boolean

---
+++ script.js
@@ -0,0 +1,15 @@
+const myName = "Jordan Gaza";
+const age = 26;
+const birthday = "December 2nd";
+console.log(
+  "Hello, my name is " + myName + ". I am currently " + age + " years old and my birthday is on " + birthday
+);
+
+const interestingFacts1 = "I was born in Big Rapids, Michigan. ";
+const interestingFacst2 = "I graduated from Kendall College of Art and Design. ";
+const interstingFacts3 = "I can carry three full drinks in one hand. ";
+const interestingFacts4 = "I am not very comfortable with JavaScript at all, but I hope to improve in this afterhours bootcamp.";
+console.log (
+    interestingFacts1 + interestingFacst2 + interstingFacts3 + interestingFacts4
+);
+
