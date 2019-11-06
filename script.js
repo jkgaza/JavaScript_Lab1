d55cab6e38f3e76f0f3276cff99c1107fdc56593
Jordan Gaza <Gaza>
2019-11-06 15:13:53 -0500

    Maybe figuring out the random number generator???

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
-
-
 const name = "Jordan Gaza";
 const detroitGC = false;
 const age = 26;
 const birthday = "December 2nd";
-function greeting(){
-  if (detroitGC == true){
-    return "Hello, my name is " + name + " and I am a student at Grand Circus in Detroit, Michigan. I am " + age + " years old and my birthday is on " + birthday;
-  } else{
-    return "Hello, my name is " + name + " and I am a student at Grand Circus in Grand Rapids, Michigan. I am " + age + " years old and my birthday is on " + birthday;
+function greeting() {
+  if (detroitGC == true) {
+    return (
+      "Hello, my name is " +
+      name +
+      " and I am a student at Grand Circus in Detroit, Michigan. I am " +
+      age +
+      " years old and my birthday is on " +
+      birthday
+    );
+  } else {
+    return (
+      "Hello, my name is " +
+      name +
+      " and I am a student at Grand Circus in Grand Rapids, Michigan. I am " +
+      age +
+      " years old and my birthday is on " +
+      birthday
+    );
   }
 }
 console.log(greeting());
 
-
-
+////
 
 const interestingFacts1 = "I was born in Big Rapids, Michigan. ";
-const interestingFacst2 = "I graduated from Kendall College of Art and Design. ";
+const interestingFacst2 =
+  "I graduated from Kendall College of Art and Design. ";
 const interstingFacts3 = "I can carry three full drinks in one hand. ";
-const interestingFacts4 = "I am not very comfortable with JavaScript at all, but I hope to improve in this afterhours bootcamp.";
-console.log (
-    interestingFacts1 + interestingFacst2 + interstingFacts3 + interestingFacts4
+const interestingFacts4 =
+  "I am not very comfortable with JavaScript at all, but I hope to improve in this afterhours bootcamp.";
+console.log(
+  interestingFacts1 + interestingFacst2 + interstingFacts3 + interestingFacts4
 );
 
 //working on random number if else//
-const randomNumber = "";
-console.log(
-  Math.floor(Math.random() * 11))
-
- //For loop list// 
-
-  for (i = 0; i < 5; i++){
-    console.log("The number is " + i) ;
+const randomNumber = Math.floor(Math.random() * 11);
+function random () {
+  if (randomNumber == 5){
+    return (
+      randomNumber + "===5"
+    );
+  } else {
+    return (
+     randomNumber + "!==5"
+    );
   }
+}
+console.log(random());
+console.log(Math.floor(Math.random() * 11));
 
- 
-//weird randomizing for loop?//
+//For loop list//
 
-//   var numbers = [
-//     console.log(
-//     Math.floor(Math.random() * 11))
-//   ];
-// var i = 5;
-// var text = "";
+var text = "";
+var i;
+for (i = 0; i < 5; i++) {
+  text += "The number is " + i;
+}
+console.log;
 
-// for (; i < len; i++) {
-//   text += numbers[i] + "<br>";
 
-//random number generator//  
+//random number generator//
 
-console.log(
-  Math.floor(Math.random() * 11))
+console.log(Math.floor(Math.random() * 11));
