854a1d6d03cc45701ae05700addd3031b4d66ba3
Jordan Gaza <Gaza>
2019-11-06 19:39:54 -0500

    cleaning up code a bit

--- script.js
+++ script.js
@@ -27,26 +27,29 @@ console.log(greeting());
 
 //trying part 2 again!!//
 function factsFunction() {
-  let facts = ["I was born in Big Rapids, Michigan. ", "I graduated from Kendall College of Art and Design. ", "I can carry three full drinks in one hand. ", "I am not very comfortable with JavaScript at all, but I hope to improve in this afterhours bootcamp."];
+  let facts = [
+    "I was born in Big Rapids, Michigan. ",
+    "I graduated from Kendall College of Art and Design. ",
+    "I can carry three full drinks in one hand. ",
+    "I am not very comfortable with JavaScript at all, but I hope to improve in this afterhours bootcamp."
+  ];
   let text = "";
   let i;
-  for (i = 0; i < facts.length; i++)
-    text += facts[i];
-    return text;
-  }
-  console.log(factsFunction());
+  for (i = 0; i < facts.length; i++) text += facts[i];
+  return text;
+}
+console.log(factsFunction());
 
 //random number generator//
 
 let counter = 0;
-while (true){
-  let randNumber = Math.floor(Math.random() * 11)
-  if (randNumber === 5){
-    console.log ("5===5 It took " + counter + " iterations to equal 5")
+while (true) {
+  let randNumber = Math.floor(Math.random() * 11);
+  if (randNumber === 5) {
+    console.log("5===5 It took " + counter + " iterations to equal 5");
     break;
-  }
-  else {
-    counter++
-    console.log (randNumber + "!==5");
+  } else {
+    counter++;
+    console.log(randNumber + "!==5");
   }
 }
